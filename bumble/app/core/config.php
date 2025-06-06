41d31fd198253f4992f1268a59b6758da93aabaa
sheesh <157548415+noeldtee@users.noreply.github.com>
2025-06-06 14:36:02 +0800

    Final
    Final

--- bumble/app/core/config.php
+++ bumble/app/core/config.php
@@ -2,8 +2,8 @@
 
 define('APP_NAME', 'BumbleWash');
 
-define('ROOT', 'http://localhost/BSIS3G_WEBA_24/bumble/public');
-define('PATH', 'C:/xampp/htdocs/BSIS3G_WEBA_24/bumble/app/views/');
+define('ROOT', 'http://localhost/bumblewash-scheduling-system/bumble/public');
+define('PATH', 'C:/xampp/htdocs/bumblewash-scheduling-system/bumble/app/views/');
 
 
 define('DB_HOST', 'localhost');
